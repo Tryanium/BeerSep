6024b7e97a3573e3446bb1abbcbb037c007fe260
marin.mouscadet <marin.mouscadet@ibm.com>
2019-04-09 14:14:09 +0200

    test API Twitter

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+const session = require('express-session');
 var TwitterStrategy = require('passport-twitter').Strategy;
 
-var env = require('../twitter-admin.json');
+router.use(session({ secret: 'SECRET' })); // session secret
+router.use(session()); // persistent login sessions
 
+var env = require('../twitter-admin.json');
 
 passport.use(new TwitterStrategy({
     consumerKey: env.consumerKey,
@@ -13,21 +16,21 @@ passport.use(new TwitterStrategy({
   },
 
   function(token, tokenSecret, profile, cb) {
-    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+    User.findOrCreate({
+      twitterId: profile.id
+    }, function(err, user) {
       return cb(err, user);
     });
   }
 ));
 
-
-router.get('/auth/twitter',
-  passport.authenticate('twitter'));
-
-router.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
+router.get('/auth/twitter', function(req, res) {
+  passport.authenticate('twitter');
+  res.send('OKAY');
 });
+
+router.get('/index',
+  passport.authenticate('twitter', { successRedirect: '/index',
+                                     failureRedirect: '/login' }));
 
 module.exports = router;
