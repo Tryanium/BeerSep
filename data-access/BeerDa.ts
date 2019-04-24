c58b666a6edc23c438f78a599d973c5a70a5ee8a
yixicheng <chengyixi1017@gmail.com>
2019-04-25 01:06:28 +0200

    ajout_beer
    ajout_beer

--- data-access/BeerDa.ts
+++ data-access/BeerDa.ts
@@ -1,19 +1,64 @@
-class BeerDA extends DataAccess {
-    private _collection;
+var admin = require("firebase-admin");
+class BeerDA {
 
     constructor() {
-        super();
-        this.collection = this.database.collection("beers");
+        require('dotenv').config();
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                "id": process.env.FIREBASE_ID,
+                "alcoholVol": process.env.FIREBASE_ALCOHOLVOL,
+                "colour": process.env.FIREBASE_COLOUR,
+                "name": process.env.FIREBASE_NAME
+            }),
+            databaseURL: "https://beersep-cf1ad.firebaseio.com"
+        });
+    }
+    static getBeer(BeerForm, callback) {
+        const database = admin.firestore();
+        database.collection("name").doc(BeerForm.Name).get()
+            .then(function(doc) {
+                if (doc.exists) {
+                    callback(doc.data());
+                } else {
+                    console.log("No such document!");
+                    callback(null);
+                }
+            }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+    }
+    static addBeer(BeerForm){
+        const database = admin.firestore();
+        let data = {
+            name: BeerForm.Name,
+            id: BeerForm.Id,
+            alcoholVol: BeerForm.AlcoholVol,
+            colour: BeerForm.Colour,
+        };
+        database.collection("id").doc(BeerForm.Id).set(data);
+        return true;
     }
 
+    static deleteBeer(BeerForm){
+        const database = admin.firestore();
 
-
-
-    get collection() {
-        return this._collection;
+        database.collection("id").doc(BeerForm.Id).remove();
+        return true;
     }
+    /*
+      addBeer() {
+      var docRef = env.collection('beer').doc('HK');
+      var setAda = docRef.set({
+        name: 'Heineken',
+        pourcentage: "5"
+      });
+      }
 
-    set collection(value) {
-        this._collection = value;
-    }
+      deleteBeer() {
+
+      }
+    }*/
+
 }
+
+module.exports = BeerDA;
--- routes/beers.js
+++ routes/beers.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
+var admin = require("firebase-admin");
 // const admin = require("firebase-admin");
 
 /* GET home page. */
 router.post('/addBeer', function(req, res) {
-  res.send('YOUPI');
+  BeerDA.addBeer(req);
+  res.send("Beer added");
+
 });
 
 router.delete('/deleteBeer', function(req, res, next) {
-  // TODO: Écrire la route
+  BeerDA.deleteBeer(req);
+  res.send("Beer deleted");
 });
 
 router.put('/updateBeer', function(req, res, next) {
-  // TODO: Écrire la route
+  BeerDA.addBeer(req);
+  res.send("Beer updated");
 });
 
 module.exports = router;
