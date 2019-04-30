ddffddbcdc984cb9d6727620942f924822c8c3dc
marin.mouscadet <marin.mouscadet@ibm.com>
2019-04-30 17:09:51 +0200

    Changement API

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', null);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
