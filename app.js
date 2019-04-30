6a0f0b4860b0e8b0b32d56350f117cd5c2aee45d
marin.mouscadet <marin.mouscadet@ibm.com>
2019-04-30 17:05:35 +0200

    Changement API

--- app.js
+++ app.js
@@ -24,14 +24,29 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', null);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
 
@@ -45,7 +60,9 @@ app.get('/testGET', function (req, res, next) {
 });
 
 app.post('/testPOST', function (req, res, next) {
+  console.log(req);
   let body = req.body;
+  console.log(body);
   res.send(body);
 });
 
