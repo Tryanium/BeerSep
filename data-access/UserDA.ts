485748b28500c7fdde005b78dd44840a9c5dbbb3
marin.mouscadet <marin.mouscadet@ibm.com>
2019-05-07 14:30:40 +0200

    nettoyage des DAs

--- data-access/UserDA.ts
+++ data-access/UserDA.ts
@@ -3,18 +3,6 @@ var admin = require("firebase-admin");
 class UserDA {
 
     constructor() {
-      /*
-      require('dotenv').config();
-      admin.initializeApp({
-        credential: admin.credential.cert({
-          "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-          "project_id": process.env.FIREBASE_PROJECT_ID,
-          "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID
-        }),
-        databaseURL: "https://beersep-cf1ad.firebaseio.com"
-      });
-      */
     }
 
     getUser(TwitterProfil, callback) {
