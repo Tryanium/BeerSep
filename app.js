92ba8a6f5a310b2fe43905e70422b83d2bda6719
marin.mouscadet <marin.mouscadet@ibm.com>
2019-05-16 18:00:19 +0200

    erreur bête de ma part

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 
 require('dotenv').config();
-app.use(cors());
 const users = require('./routes/users');
 const beers = require('./routes/beers');
 const connection = require('./routes/connection');
@@ -33,6 +32,7 @@ admin.initializeApp({
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json({
   limit: '50mb'
