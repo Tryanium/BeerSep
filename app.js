618c488f187d7ad99eb4b3fc23a52a3dad9ec969
marin.mouscadet <marin.mouscadet@ibm.com>
2019-05-16 18:28:28 +0200

    addBeer

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json({
   limit: '50mb'
 }));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/users', users);
